a5a2588a8740f27b8aa6dfff7e53b14f6fbf9249
smateega <81055422+smateega@users.noreply.github.com>
2021-12-30 14:44:17 -0600

    folder commit error

--- lockedsite/test folder/click.js
+++
@@ -1,9 +0,0 @@
-const contact = document.querySelector('.contact');
-const linkedin = document.querySelector('.linkedin');
-
-contact.addEventListener('click', () => {
-  contact.classList.toggle('active');
-  linkedin.classList.toggle('active');
-})
-
-
